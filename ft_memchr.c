f84113ab6a5bea67bbf0d8b2377958984f18ee76
Bugra Oksuz <boksuz@k1m33s07.42istanbul.com.tr>
2025-06-16 19:45:04 +0300

    libft is not finished

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: boksuz <boksuz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 15:44:27 by boksuz            #+#    #+#             */
+/*   Updated: 2025/06/12 15:50:28 by boksuz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lifbt.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	size_t			i;
+	unsigned char	uc;
+
+	str = (unsigned char *) s;
+	uc (unsigned char) c;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == uc)
+			return ((void *) &str[i]);
+		i++;
+	}
+	return (NULL);
+}
